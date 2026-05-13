7437fbfddcd1ca14af96d72581de4f885fee30be
oscarfg78 <oscarfg78@gmail.com>
2026-05-13 02:35:58 -0600

    fix: deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,6 @@ scripts/replace-placeholder.sh "$BUILT_NEXT_PUBLIC_WEBAPP_URL" "$NEXT_PUBLIC_WEB
 if [ -n "$DATABASE_HOST" ]; then
   scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 fi
-npx prisma migrate deploy --schema /calcom/packages/prisma/schema.prisma
-npx ts-node --transpile-only /calcom/scripts/seed-app-store.ts
+npx prisma db push --accept-data-loss --schema /calcom/packages/prisma/schema.prisma
+# npx ts-node --transpile-only /calcom/scripts/seed-app-store.ts
 yarn start
